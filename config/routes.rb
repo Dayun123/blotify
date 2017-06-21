3d5597f3303826d73f0a5aee58d7fa321f4d594e
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-20 20:32:49 -0500

    Add welcome controller, navbar, root route, and logo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   # TODO: Must declare a root for devise to work
-  # root 'something#index'
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
